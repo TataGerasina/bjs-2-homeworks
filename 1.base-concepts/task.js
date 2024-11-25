4555c263e43fcf63b8044c3fe13eebcbc0f7b3f6
Tatyana Gerasina <gtstata@gmail.com>
2024-11-25 17:28:02 +0300

    The first task has been completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+let a = +prompt("Введите A:"),
+b = +prompt("Введите B:"),
+c = +prompt("Введите C:"),
+arr = solveEquation(a, b, c),
+d = arr[0];
+console.log("Дискриминант: " + d);
+console.log(
+d < 0
+    ? "Корней нет"
+    : d === 0
+        ? "Один корень: " + arr[1]
+        : `Два корня: ${arr[1]} и ${arr[2]}`
+);
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+let discr = (a, b, c) => Math.pow(b, 2) - 4*a*c;
+
+let d = discr(a, b, c);
+if (d < 0) return [d];
+
+let x1 = (-b + Math.sqrt(d)) / (2 * a),
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+return [d, x1, x2];
 }
