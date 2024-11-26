6023ba43638103e020bf5d696dd72094ea69409b
Tatyana Gerasina <gtstata@gmail.com>
2024-11-26 10:56:27 +0300

    The first task has been corrected

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,26 +1,25 @@
 "use strict"
-let a = +prompt("Введите A:"),
-b = +prompt("Введите B:"),
-c = +prompt("Введите C:"),
-arr = solveEquation(a, b, c),
-d = arr[0];
-console.log("Дискриминант: " + d);
-console.log(
-d < 0
-    ? "Корней нет"
-    : d === 0
-        ? "Один корень: " + arr[1]
-        : `Два корня: ${arr[1]} и ${arr[2]}`
-);
-
+let a = +prompt;
+b = +prompt;
+c = +prompt;
 function solveEquation(a, b, c) {
-let discr = (a, b, c) => Math.pow(b, 2) - 4*a*c;
-
+let arr = [];
+let discr = (a, b, c) => b ** 2 - 4 * a * c;
 let d = discr(a, b, c);
-if (d < 0) return [d];
 
-let x1 = (-b + Math.sqrt(d)) / (2 * a),
-    x2 = (-b - Math.sqrt(d)) / (2 * a);
-
-return [d, x1, x2];
+    if(d === 0) {
+        let x = -b / (2 * a);
+        arr.push(x);
+        console.log(arr[x])
+    }
+    else if(d > 0) {   
+    let x1 = (-b + Math.sqrt(d)) / (2 * a),
+        x2 = (-b - Math.sqrt(d)) / (2 * a);
+        arr.push(x1, x2);
+        console.log(`${arr[x1]} и ${arr[x2]}`) 
+    }
+    else if(d < 0) {
+        console.log(arr[0])
+    }
+  return arr;
 }
