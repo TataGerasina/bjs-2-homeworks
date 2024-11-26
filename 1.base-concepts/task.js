719343731516cded14cc38919a7829f75e99df52
Tatyana Gerasina <gtstata@gmail.com>
2024-11-26 11:20:57 +0300

    The first task has been solved beautiful

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,24 +2,23 @@
 let a = +prompt;
 let b = +prompt;
 let c = +prompt;
-function solveEquation(a, b, c) {
-let arr = [];
-let discr = (a, b, c) => b ** 2 - 4 * a * c;
-let d = discr(a, b, c);
 
-    if(d === 0) {
-        let x = -b / (2 * a);
-        arr.push(x);
-        console.log(arr[x])
-    }
-    else if(d > 0) {   
-    let x1 = (-b + Math.sqrt(d)) / (2 * a),
-        x2 = (-b - Math.sqrt(d)) / (2 * a);
-        arr.push(x1, x2);
-        console.log(`${arr[x1]} и ${arr[x2]}`) 
-    }
-    else if(d < 0) {
-        console.log(arr[0])
-    }
-  return arr;
+function solveEquation(a, b, c) {
+	let arr = [];
+	let discr = (a, b, c) => b ** 2 - 4 * a * c;
+	let d = discr(a, b, c);
+
+	if (d === 0) {
+		let x = -b / (2 * a);
+		arr.push(x);
+		console.log(arr[x])
+	} else if (d > 0) {
+		let x1 = (-b + Math.sqrt(d)) / (2 * a),
+			x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1, x2);
+		console.log(`${arr[x1]} и ${arr[x2]}`)
+	} else if (d < 0) {
+		console.log(arr[0])
+	}
+	return arr;
 }
