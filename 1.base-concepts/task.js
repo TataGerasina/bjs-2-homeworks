64c35f8cadfa49610577b1902dbeca072396254e
Tatyana Gerasina <gtstata@gmail.com>
2024-11-29 16:47:53 +0300

    The second task has been solved beautiful

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -36,6 +36,6 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	let monthlyPayment = bodyCredit * (monthlyPercent + (monthlyPercent / ((Math.pow((1 + monthlyPercent)) - 1))));
 	let totalAmount = monthlyPayment * countMonths;
   totalAmount.toFixed(2);
-  Number(totalAmount.toFixed(2));
-	return Number(totalAmount.toFixed(2)) ;
+  Number.isNaN(totalAmount.toFixed(2))
+	return totalAmount;
 }
