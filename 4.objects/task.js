ea70f28636b92dcdd0b410533c1a08be9bbeabfe
Tatyana Gerasina <gtstata@gmail.com>
2025-01-09 13:01:26 +0300

    The fourth task is solved

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,58 @@
 function Student(name, gender, age) {
-  
-}
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+	Student.prototype.setSubject = function(subjectName) {
+		this.subject = subjectName;
+	}
 
-Student.prototype.getAverage = function () {
-  
-}
+	Student.prototype.addMarks = function(...marksToAdd) {
+		if (!this.marks) {
+			console.log("Error: Student has been expelled.");
+			return;
+		}
 
-Student.prototype.exclude = function (reason) {
-  
+		if (!!this.marksToAdd.lenght) {
+			this.marks.push(...marksToAdd);
+		}
+
+		return;
+	}
+
+	Student.prototype.getAverage = function() {
+
+		if (!this.marks || !this.marks.length) {
+			return 0;
+		}
+		const sum = this.marks.reduce((acc, curr) => acc + curr);
+		return sum / this.marks.length;
+	}
+
+	Student.prototype.exclude = function(reason) {
+		delete this.subject;
+		delete this.marks;
+		this.excluded = reason;
+	}
+
+	let student1 = new Student("Василиса", "женский", 19);
+	student1.setSubject("Algebra");
+	console.log(student1.getAverage()); // 0
+	student1.addMarks(4, 5, 4, 5);
+	console.log(student1.getAverage()); // 4.5
+	console.log(student1);
+	// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+	let student2 = new Student("Артём", "мужской", 25);
+	student2.setSubject("Geometry");
+	student2.exclude('плохая учёба')
+	console.log(student2)
+	// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
+	let student3 = new Student("Андрей", "мужской", 28);
+	student3.setSubject("History");
+	student1.addMarks(2, 3, 2, 2);
+	student3.exclude('плохая учёба')
+	console.log(student3)
+	// {name: "Андрей", gender: "мужской", age: 28, excluded: "плохая учёба"}
 }
