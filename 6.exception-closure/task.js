f34c1802c7b380d4928d59b64021311a3257f88e
Tatyana Gerasina <gtstata@gmail.com>
2025-01-25 12:56:42 +0300

    The task 1 solved

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,22 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    
+    if(Number.isNaN(parsedValue)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsedValue;
+}
+
+const parsedValue = parseCount("67,8")
+console.log(typeof parsedValue);
+
+function validateCount(value) {
+    try {
+        const parsedValue = parseCount(value);
+        return parsedValue;
+    } catch (error) {
+        return error;
+    }
+}
+const invalidValue = validateCount("string");
+console.log(invalidValue.message);
